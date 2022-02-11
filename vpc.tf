3418f85109bacaf059af932d92da10e778dc8fb7
Your Name <you@example.com>
2022-02-11 22:10:00 +0000

    Added the second label

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module """ {
     version = "~> 3.0"
 
     project_id   = var.project
-    network_name = "example-vpc"
+    network_name = "gaurav-network"
     routing_mode = "GLOBAL"
 
     subnets = [
@@ -12,23 +12,6 @@ module """ {
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = "us-west1"
         },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-        }
     ]
 
     secondary_ranges = {
